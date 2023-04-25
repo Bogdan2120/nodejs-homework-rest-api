21ac6024bab2c52be14eb8fdd127ce4fbe7d3521
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2023-04-25 13:38:55 +0300

    hw05-avatars

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/auth-controllers");
 
-const { authentivate } = require("../../midlewares");
+const { authentivate, upload } = require("../../midlewares");
 const { validateBody } = require("../../utils");
 const { schemas } = require("../../models/user");
 
@@ -11,5 +11,10 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authentivate, ctrl.getCurrent);
 router.post("/logout", authentivate, ctrl.logout);
-
+router.patch(
+  "/avatars",
+  authentivate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
+);
 module.exports = router;
